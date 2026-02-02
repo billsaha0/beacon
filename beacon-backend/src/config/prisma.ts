4445b75058d4d7b35c41fecce1db19492fa1d12d
billsaha0 <billsaha0@gmail.com>
2026-02-02 19:59:44 +0530

    Update prisma.ts

--- beacon-backend/src/config/prisma.ts
+++ beacon-backend/src/config/prisma.ts
@@ -8,7 +8,7 @@ if (!connectionString) {
     throw new Error("DATABASE_URL is not defined")
 }
 
-const caCert = process.env.DATABASE_CA;
+const caCert = Buffer.from(process.env.DATABASE_CA_BASE64!, "base64").toString("utf-8");
 
 const adapter = new PrismaPg({
     connectionString,
